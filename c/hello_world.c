38fea6405b4174393b50d19aa644ded20cbd0453
Nick Fitzgerald <fitzgen@gmail.com>
2017-02-28 00:04:04 -0800

    Add more example kinds of FFI

--- c/hello_world.c
+++ c/hello_world.c
@@ -1,5 +1,16 @@
+#include <stdint.h>
 #include <stdio.h>
 
 void hello_world() {
     printf("Hello, World!\n");
+}
+
+void hello_to(const char* who, size_t times) {
+    for (size_t i = 0; i < times; i++) {
+        printf("Hello, %s!\n", who);
+    }
+}
+
+size_t super_fast_add(size_t a, size_t b) {
+    return a + b;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+extern crate libc;
+
 extern "C" {
     fn hello_world();
+
+    fn hello_to(who: *const libc::c_char, times: libc::size_t);
+
+    fn super_fast_add(a: libc::size_t, b: libc::size_t) -> libc::size_t;
 }
 
 fn main() {
     unsafe {
         hello_world();
+
+        let who = b"PDXRust\0";
+        hello_to(who.as_ptr() as *const _, 3);
+
+        let sum = super_fast_add(4, 38);
+        println!("sum of 4 and 38 is {}", sum);
     }
 }
